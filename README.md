c8f108436b6abddb387a839658d5ad7737e117fc
anonym <anonym@anonym>
2024-01-10 23:07:48 +0100

    script to clone multiple repo

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# clone_multi_repo
-This is a script to compile multiple repo on github
+# Multi-Repo Cloning Script
+
+## Overview
+
+This Bash script is designed to automate the process of cloning multiple GitHub repositories simultaneously. 
+It reads a list of repository names from a text file and clones each repository using the SSH protocol.
+
+## Prerequisites
+
+Before using this script, make sure you have the following prerequisites:
+
+- Git installed on your machine
+- GitHub account with SSH keys configured
+
+## Usage
+
+1. Create a text file (`file_name.txt`) containing the names of the GitHub repositories, with each name on a new line.
+
+2. Run the script by executing the following command in the terminal:
+
+   ```bash
+   ./clone_repos.sh
+
+3. Ensure that the script file (clone_repos.sh) has executable permissions:
+
+    ```bash
+    chmod +x clone_repos.sh
---
+++ clone_repo.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Specify the path of the text file containing student names
+input_file="file_name.txt"
+
+# Make sure the file exists
+if [ ! -f "$input_file" ]; then
+    echo "The file $input_file does not exist."
+    exit 1
+fi
+
+# Read the file line by line and clone the repos
+while IFS= read -r repo_name; do
+   
+    # For this part, you can use the ssh link
+    repo_url="git@github.com:"username_github"/$repo_name.git"
+
+    echo "Cloning repo for $repo_name..."
+    
+    # Add a debug message to see the value of repo_url
+    echo "Repo URL: $repo_url"
+    
+    # Add a debug message to see the result of the git clone command
+    git clone "$repo_url"
+    
+    # Add a debug message to see if the git clone command succeeded or failed
+    if [ $? -eq 0 ]; then
+        echo "Cloning successful for $repo_name"
+    else
+        echo "Cloning failed for $repo_name"
+    fi
+done < "$input_file"
+
+echo "Cloning completed."
